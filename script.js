9f3e59a3dcc8f1b0203db643687d9422ae30d751
gowthamsarjah13 <119598674+gowthamsarjah13@users.noreply.github.com>
2023-02-27 22:19:00 +0530

    html,css,js

---
+++ script.js
@@ -0,0 +1,12 @@
+let resume = {
+    "firstname":"Gowtham",
+    "lastname":"sarjah",
+    "age":"23",
+    "email":"gowthamsarjah@gmail.com",
+    "mobile":"94431-40887",
+    "qualification": "Bsc.IT",
+    "university":"Alagappa university",
+    "experience":"4 years in medical billing",
+}
+
+console.log(resume);
